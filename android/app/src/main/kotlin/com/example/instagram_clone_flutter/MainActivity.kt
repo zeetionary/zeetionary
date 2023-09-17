79c55d0e72a02d9fbfedb41b400a875819cbafdc
arkansalihen2@gmail.com <arkansalihen2@gmail.com>
2023-09-17 20:49:29 +0300

    big

--- android/app/src/main/kotlin/com/example/instagram_clone_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.instagram_clone_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/zeetionary_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.zeetionary_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}

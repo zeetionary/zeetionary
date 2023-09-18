5877cb6ca9a79094f77133fc907cd800753cbcaa
arkansalihen2@gmail.com <arkansalihen2@gmail.com>
2023-09-18 15:56:30 +0300

    zero

--- lib/utils/dimensions.dart
+++
@@ -1,8 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:zeetionary_flutter/screens/add_post_screen.dart';
-import 'package:zeetionary_flutter/screens/feed_screen.dart';
-import 'package:zeetionary_flutter/screens/profile_screen.dart';
-import 'package:zeetionary_flutter/screens/search_screen.dart';
-
-const webScreenSize = 600;

41ac19b441bc2174fb251054d51b561f7f721188
arkansalihen2@gmail.com <arkansalihen2@gmail.com>
2023-09-20 21:50:28 +0300

    New

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const logoPath = 'assets/images/logo.png';
+  static const loginEmotePath = 'assets/images/logo.png';
+  static const googlePath = 'assets/images/logo.png';
+}

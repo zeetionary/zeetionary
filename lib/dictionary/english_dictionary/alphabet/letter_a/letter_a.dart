bdb25211739de24dd7cefaac454fc1c8a35c1c48
arkansalihen2@gmail.com <arkansalihen2@gmail.com>
2023-09-27 21:05:29 +0300

    some changes

--- lib/dictionary/english_dictionary/alphabet/letter_a/letter_a.dart
+++ lib/dictionary/english_dictionary/alphabet/letter_a/letter_a.dart
@@ -5,15 +5,22 @@ class EnglishEntryAardvark extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Aardvark'),
-        ),
-        body: const Center(
-          child: Text(
-              'A large, nocturnal, burrowing mammal native to central and southern Africa, with a long, extensile tongue and strong claws, which feeds on ants and termites.'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Aardvark'),
+      ),
+      body: const Center(
+        child: Text('''
+          A large, nocturnal, burrowing mammal native to 
+
+          central and southern Africa,nwith a long, extensile 
+
+          tongue and strong claws, which 
+          
+          feeds on ants 
+
+          and termites.
+          '''),
       ),
     );
   }

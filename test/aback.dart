bafcb667732c26115a6856ff3661553b56a38eab
arkansalihen2@gmail.com <arkansalihen2@gmail.com>
2023-10-18 12:44:36 +0300

    m

---
+++ test/aback.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:zeetionary/dictionary/english_dictionary/alphabet/letter_a/letters_ab/en_entry_aback.dart';
+
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/dictionaryEnglish',
+      builder: (BuildContext context, GoRouterState state) {
+        return const DictionaryScreenEnglish();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'english/:aback',
+          builder: (BuildContext context, GoRouterState state) {
+            return EnglishEntryAback();
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
+class DictionaryScreenEnglish extends StatefulWidget {
+  const DictionaryScreenEnglish({Key? key}) : super(key: key);
+
+  @override
+  State<DictionaryScreenEnglish> createState() =>
+      _DictionaryScreenEnglishState();
+}
+
+class _DictionaryScreenEnglishState extends State<DictionaryScreenEnglish> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final List<String> allWordsEnglish = [
+    "aback",
+    "abacus",
+  ];
+  List<String> filteredWords = [];
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    filteredWords = List.from(allWordsEnglish);
+  }
+
+  void filterWords(String query) {
+    setState(() {
+      filteredWords = allWordsEnglish
+          .where((word) => word.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
+  void clearSearch() {
+    _searchController.clear();
+    filterWords('');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Directionality(
+            textDirection:
+                TextDirection.rtl, // Set the text direction to right-to-left
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                height: 60,
+                child: TextField(
+                  controller: _searchController,
+                  onChanged: filterWords,
+                  decoration: InputDecoration(
+                    labelText: 'Search here',
+                    prefixIcon: const Icon(Icons.search),
+                    suffixIcon: IconButton(
+                      icon: const Icon(Icons.clear),
+                      onPressed: clearSearch,
+                    ),
+                    border: const OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Directionality(
+              textDirection:
+                  TextDirection.rtl, // Set the text direction to right-to-left
+              child: EnglishDictionary(
+                words: filteredWords,
+                onTapWord: (wordsEnglish) {
+                  if (wordsEnglish == "aback") {
+                    context.go('english/:aback');
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+}
+
+class EnglishDictionary extends StatelessWidget {
+  final List<String> words;
+  final Function(String) onTapWord;
+
+  const EnglishDictionary({
+    Key? key,
+    required this.words,
+    required this.onTapWord,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: words.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTileEnglish(
+          wordsEnglish: words[index],
+          onTap: () {
+            onTapWord(words[index]);
+          },
+        );
+      },
+    );
+  }
+}
+
+class ListTileEnglish extends StatelessWidget {
+  final String wordsEnglish;
+  final VoidCallback? onTap;
+
+  const ListTileEnglish({
+    Key? key,
+    required this.wordsEnglish,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: ListTile(
+        key: key,
+        title: Text(wordsEnglish),
+        trailing: const Icon(Icons.arrow_forward),
+      ),
+    );
+  }
+}
+
+class CardButton extends StatelessWidget {
+  final String label;
+  final VoidCallback? onPressed;
+
+  const CardButton({
+    Key? key,
+    required this.label,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: Card(
+        child: InkWell(
+          onTap: onPressed,
+          child: Center(
+            child: Text(
+              label,
+              style: const TextStyle(fontSize: 16),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

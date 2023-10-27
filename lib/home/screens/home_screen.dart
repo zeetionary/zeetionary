e9584cdf9639bb99d212f74d5845bc6853a201f8
arkansalihen2@gmail.com <arkansalihen2@gmail.com>
2023-10-27 10:53:37 +0300

    commented out unnecessary import in home screen

--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:developer';
 
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zeetionary/constants.dart';

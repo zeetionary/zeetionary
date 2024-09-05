7210eb6e7da06819979abc5b44a1cfd4dc7e4d6d
zeetionary <zeetionary@gmail.com>
2024-09-05 17:41:08 +0300

    mmmm

--- test/update_others.dart
+++ test/update_others.dart
@@ -1,234 +1,234 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_tts/flutter_tts.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:zeetionary/constants.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_tts/flutter_tts.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:zeetionary/constants.dart';
 
-enum TtsState { playing }
+// enum TtsState { playing }
 
-class EnglishEntrydopsum1 extends StatefulWidget {
-  const EnglishEntrydopsum1({super.key});
+// class EnglishEntrydopsum1 extends StatefulWidget {
+//   const EnglishEntrydopsum1({super.key});
 
-  @override
-  State<EnglishEntrydopsum1> createState() => _EnglishEntrydopsum1State();
-}
+//   @override
+//   State<EnglishEntrydopsum1> createState() => _EnglishEntrydopsum1State();
+// }
 
-class _EnglishEntrydopsum1State extends State<EnglishEntrydopsum1> {
-  @override
-  void initState() {
-    super.initState();
-    flutterTts = FlutterTts();
-    flutterTts.setLanguage("en-GB");
-    flutterTts.setLanguage("en-US");
-    fetchSentences();
-  }
+// class _EnglishEntrydopsum1State extends State<EnglishEntrydopsum1> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     flutterTts = FlutterTts();
+//     flutterTts.setLanguage("en-GB");
+//     flutterTts.setLanguage("en-US");
+//     fetchSentences();
+//   }
 
-  FlutterTts flutterTts = FlutterTts();
+//   FlutterTts flutterTts = FlutterTts();
 
-  bool isSpeaking = false;
+//   bool isSpeaking = false;
 
-  Future<void> startSpeaking(
-      String languageCode, EnglishMeaningConst englishMeaningConst) async {
-    String textToSpeak = """
-${englishMeaningConst.text}
-""";
+//   Future<void> startSpeaking(
+//       String languageCode, EnglishMeaningConst englishMeaningConst) async {
+//     String textToSpeak = """
+// ${englishMeaningConst.text}
+// """;
 
-    await flutterTts.setLanguage(languageCode);
-    await flutterTts.setPitch(ttsPitch);
-    await flutterTts.setSpeechRate(ttsSpeechRate);
-    await flutterTts.speak(textToSpeak);
+//     await flutterTts.setLanguage(languageCode);
+//     await flutterTts.setPitch(ttsPitch);
+//     await flutterTts.setSpeechRate(ttsSpeechRate);
+//     await flutterTts.speak(textToSpeak);
 
-    setState(() {
-      isSpeaking = true;
-    });
-  }
+//     setState(() {
+//       isSpeaking = true;
+//     });
+//   }
 
-  Future<void> stopSpeaking() async {
-    await flutterTts.stop();
+//   Future<void> stopSpeaking() async {
+//     await flutterTts.stop();
 
-    setState(() {
-      isSpeaking = false;
-    });
-  }
+//     setState(() {
+//       isSpeaking = false;
+//     });
+//   }
 
-  final EnglishMeaningConst englishMeaningConst = const EnglishMeaningConst(
-    text: """
+//   final EnglishMeaningConst englishMeaningConst = const EnglishMeaningConst(
+//     text: """
 
-EnglishEntrydopsum1 dopsum1
-WORD_WEB dopsum1 dopsum1 dopsum1
-haʊʊʊʊʊʊʊ4
-""",
-  );
-// 188888880002200
+// EnglishEntrydopsum1 dopsum1
+// WORD_WEB dopsum1 dopsum1 dopsum1
+// haʊʊʊʊʊʊʊ4
+// """,
+//   );
+// // 188888880002200
 
-  final String keyword = "dopsum1";
-  List<Map<String, dynamic>> filteredSentences = [];
+//   final String keyword = "dopsum1";
+//   List<Map<String, dynamic>> filteredSentences = [];
 
-  Future<void> fetchSentences() async {
-    final sentences =
-        await DatabaseUtils.instance.fetchFilteredSentences(keyword: keyword);
-    setState(() {
-      filteredSentences = sentences;
-    });
-  }
+//   Future<void> fetchSentences() async {
+//     final sentences =
+//         await DatabaseUtils.instance.fetchFilteredSentences(keyword: keyword);
+//     setState(() {
+//       filteredSentences = sentences;
+//     });
+//   }
 
-  void speakEnglish(String text, {String? languageCode}) async {
-    await flutterTts.setLanguage(languageCode ?? "en-GB");
-    await flutterTts.speak(text);
-  }
+//   void speakEnglish(String text, {String? languageCode}) async {
+//     await flutterTts.setLanguage(languageCode ?? "en-GB");
+//     await flutterTts.speak(text);
+//   }
 
-  Future<void> speakheadword(String languageCode) async {
-    await flutterTts.setLanguage(languageCode);
-    await flutterTts.setPitch(ttsPitch);
-    await flutterTts.setSpeechRate(ttsSpeechRate);
-    await flutterTts.speak("""dopsum1""");
-  }
+//   Future<void> speakheadword(String languageCode) async {
+//     await flutterTts.setLanguage(languageCode);
+//     await flutterTts.setPitch(ttsPitch);
+//     await flutterTts.setSpeechRate(ttsSpeechRate);
+//     await flutterTts.speak("""dopsum1""");
+//   }
 
-  @override
-  Widget build(BuildContext context) {
+//   @override
+//   Widget build(BuildContext context) {
 
-    // 188888880002200
-    const String videoIdend = '';
-    const double startSecondsend = 000000000000000000000000000;
-    const String videoIdone = '';
-    const double startSecondsone = 000000000000000000000000000;
-    const String videoIdtwo = '';
-    const double startSecondstwo = 000000000000000000000000000;
-    const String videoIdthree = '';
-    const double startSecondsthree = 000000000000000000000000000;
-    const String videoIdfour = '';
-    const double startSecondsfour = 000000000000000000000000000;
-    const String videoIdfive = '';
-    const double startSecondsfive = 000000000000000000000000000;
+//     // 188888880002200
+//     const String videoIdend = '';
+//     const double startSecondsend = 000000000000000000000000000;
+//     const String videoIdone = '';
+//     const double startSecondsone = 000000000000000000000000000;
+//     const String videoIdtwo = '';
+//     const double startSecondstwo = 000000000000000000000000000;
+//     const String videoIdthree = '';
+//     const double startSecondsthree = 000000000000000000000000000;
+//     const String videoIdfour = '';
+//     const double startSecondsfour = 000000000000000000000000000;
+//     const String videoIdfive = '';
+//     const double startSecondsfive = 000000000000000000000000000;
 
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: const ZeetionaryAppbar(),
-        body: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) {
-            return [
-              CustomSliverAppBar(
-                flexibleSpace: FlexibleSpaceBar(
-                  background: SingleChildScrollView(
-                    child: EntryPageColumn(
-                      word: """dopsum1""",
-                      // alsoEnglishWord: "also: dopsum1",
-                      britshText: """IpaUK: haʊʊʊʊʊʊʊ4""",
-                      americanText: """IpaUS: haʊʊʊʊʊʊʊ4""",
-                      onPressedBritish: () => speakheadword("en-GB"),
-                      onPressedAmerican: () => speakheadword("en-US"),
-                    ),
-                  ),
-                ),
-                bottom: const CustomTabBarNew(
-                  tabs: [
-                    UkIconForTab(),
-                    KurdIconForTab(),
-                    SentencesIconForTab(),
-                    VideoIconForTab(),
-                  ],
-                ),
-              ),
-            ];
-          },
-          body: TabBarView(
-            children: [
-              SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    EnglishButtonTTS(
-                      onBritishPressed: (languageCode) =>
-                          startSpeaking(languageCode, englishMeaningConst),
-                      onAmericanPressed: (languageCode) =>
-                          startSpeaking(languageCode, englishMeaningConst),
-                      onStopPressed: stopSpeaking,
-                    ),
-                    englishMeaningConst,
-                  ],
-                ),
-              ),
-              SingleChildScrollView(
-                child: CustomColumnWidget(
-                  children: [
-                    
-                  ],
-                ),
-              ),
-              Consumer(
-                builder: (context, ref, child) {
-                  if (filteredSentences.isEmpty) {
-                    return const NoSentencesFromDatabase();
-                  } else {
-                    return ListView.builder(
-                      itemCount: filteredSentences.length,
-                      itemBuilder: (context, index) {
-                        final sentence = filteredSentences[index];
-                        final showDivider = filteredSentences.length > 1 &&
-                            index != filteredSentences.length - 1;
-                        return CustomSentenceWidget(
-                          englishText: sentence['english'].toString(),
-                          frenchText: sentence['french'].toString(),
-                          keyword: keyword,
-                          onPressedBritish: () => speakEnglish(
-                            sentence['english'].toString(),
-                            languageCode: "en-GB",
-                          ),
-                          onPressedAmerican: () => speakEnglish(
-                            sentence['english'].toString(),
-                            languageCode: "en-US",
-                          ),
-                          showDivider: showDivider,
-                        );
-                      },
-                    );
-                  }
-                },
-              ),
-              const YouTubeScroller(
-                children: [
-                  YoutubeEmbeddingWidget(
-                    key: ValueKey(videoIdend),
-                    videoId: videoIdend,
-                    startSeconds: startSecondsend,
-                  ),
-                  YoutubeEmbeddingWidget(
-                    key: ValueKey(videoIdone),
-                    videoId: videoIdone,
-                    startSeconds: startSecondsone,
-                  ),
-                  YoutubeEmbeddingWidget(
-                    key: ValueKey(videoIdtwo),
-                    videoId: videoIdtwo,
-                    startSeconds: startSecondstwo,
-                  ),
-                  YoutubeEmbeddingWidget(
-                    key: ValueKey(videoIdthree),
-                    videoId: videoIdthree,
-                    startSeconds: startSecondsthree,
-                  ),
-                  YoutubeEmbeddingWidget(
-                    key: ValueKey(videoIdfour),
-                    videoId: videoIdfour,
-                    startSeconds: startSecondsfour,
-                  ),
-                  YoutubeEmbeddingWidget(
-                    key: ValueKey(videoIdfive),
-                    videoId: videoIdfive,
-                    startSeconds: startSecondsfive,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+//     return DefaultTabController(
+//       length: 4,
+//       child: Scaffold(
+//         appBar: const ZeetionaryAppbar(),
+//         body: NestedScrollView(
+//           headerSliverBuilder: (context, innerBoxIsScrolled) {
+//             return [
+//               CustomSliverAppBar(
+//                 flexibleSpace: FlexibleSpaceBar(
+//                   background: SingleChildScrollView(
+//                     child: EntryPageColumn(
+//                       word: """dopsum1""",
+//                       // alsoEnglishWord: "also: dopsum1",
+//                       britshText: """IpaUK: haʊʊʊʊʊʊʊ4""",
+//                       americanText: """IpaUS: haʊʊʊʊʊʊʊ4""",
+//                       onPressedBritish: () => speakheadword("en-GB"),
+//                       onPressedAmerican: () => speakheadword("en-US"),
+//                     ),
+//                   ),
+//                 ),
+//                 bottom: const CustomTabBarNew(
+//                   tabs: [
+//                     UkIconForTab(),
+//                     KurdIconForTab(),
+//                     SentencesIconForTab(),
+//                     VideoIconForTab(),
+//                   ],
+//                 ),
+//               ),
+//             ];
+//           },
+//           body: TabBarView(
+//             children: [
+//               SingleChildScrollView(
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     EnglishButtonTTS(
+//                       onBritishPressed: (languageCode) =>
+//                           startSpeaking(languageCode, englishMeaningConst),
+//                       onAmericanPressed: (languageCode) =>
+//                           startSpeaking(languageCode, englishMeaningConst),
+//                       onStopPressed: stopSpeaking,
+//                     ),
+//                     englishMeaningConst,
+//                   ],
+//                 ),
+//               ),
+//               SingleChildScrollView(
+//                 child: CustomColumnWidget(
+//                   children: [
 
-// end WORD_WEB
+//                   ],
+//                 ),
+//               ),
+//               Consumer(
+//                 builder: (context, ref, child) {
+//                   if (filteredSentences.isEmpty) {
+//                     return const NoSentencesFromDatabase();
+//                   } else {
+//                     return ListView.builder(
+//                       itemCount: filteredSentences.length,
+//                       itemBuilder: (context, index) {
+//                         final sentence = filteredSentences[index];
+//                         final showDivider = filteredSentences.length > 1 &&
+//                             index != filteredSentences.length - 1;
+//                         return CustomSentenceWidget(
+//                           englishText: sentence['english'].toString(),
+//                           frenchText: sentence['french'].toString(),
+//                           keyword: keyword,
+//                           onPressedBritish: () => speakEnglish(
+//                             sentence['english'].toString(),
+//                             languageCode: "en-GB",
+//                           ),
+//                           onPressedAmerican: () => speakEnglish(
+//                             sentence['english'].toString(),
+//                             languageCode: "en-US",
+//                           ),
+//                           showDivider: showDivider,
+//                         );
+//                       },
+//                     );
+//                   }
+//                 },
+//               ),
+//               const YouTubeScroller(
+//                 children: [
+//                   YoutubeEmbeddingWidget(
+//                     key: ValueKey(videoIdend),
+//                     videoId: videoIdend,
+//                     startSeconds: startSecondsend,
+//                   ),
+//                   YoutubeEmbeddingWidget(
+//                     key: ValueKey(videoIdone),
+//                     videoId: videoIdone,
+//                     startSeconds: startSecondsone,
+//                   ),
+//                   YoutubeEmbeddingWidget(
+//                     key: ValueKey(videoIdtwo),
+//                     videoId: videoIdtwo,
+//                     startSeconds: startSecondstwo,
+//                   ),
+//                   YoutubeEmbeddingWidget(
+//                     key: ValueKey(videoIdthree),
+//                     videoId: videoIdthree,
+//                     startSeconds: startSecondsthree,
+//                   ),
+//                   YoutubeEmbeddingWidget(
+//                     key: ValueKey(videoIdfour),
+//                     videoId: videoIdfour,
+//                     startSeconds: startSecondsfour,
+//                   ),
+//                   YoutubeEmbeddingWidget(
+//                     key: ValueKey(videoIdfive),
+//                     videoId: videoIdfive,
+//                     startSeconds: startSecondsfive,
+//                   ),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// // end WORD_WEB
 
 
-// ZeetionaryAppbar(),
-//         body: Padding(
+// // ZeetionaryAppbar(),
+// //         body: Padding(
